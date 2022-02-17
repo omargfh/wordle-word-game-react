02e3cfb50a920d5de5709ed7a85549d4d5c3ddb7
Omar Ibrahim <omaribrahim@cs.uchicago.edu>
2022-02-17 17:41:11 -0600

    domain

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -19,10 +19,10 @@ const Sidebar = () => {
           <PersonalLogo />
             <div className='sidebar-list'>
               <ul>
-                <SidebarItem img="\home.png" href="http://www.omaribrahim.tech" sub="Go Home"/>
-                <SidebarItem img="\lg.png" href="http://wordle.omaribrahim.tech" sub="Wordle" active={true}/>
-                <SidebarItem img="http://mazebuilder.omaribrahim.tech/block.png"href="http://mazebuilder.omaribrahim.tech" sub="Maze Builder"/>
-                <SidebarItem img="\ayat.png" href="http://ayat.omaribrahim.tech" sub="Ayat Search"/>
+                <SidebarItem img="\home.png" href="http://www.omar-ibrahim.com" sub="Go Home"/>
+                <SidebarItem img="\lg.png" href="http://wordle.omar-ibrahim.com" sub="Wordle" active={true}/>
+                <SidebarItem img="http://mazebuilder.omar-ibrahim.com/block.png"href="http://mazebuilder.omar-ibrahim.com" sub="Maze Builder"/>
+                <SidebarItem img="\ayat.png" href="http://ayat.omar-ibrahim.com" sub="Ayat Search"/>
               </ul>
             </div>
             <div className='sidebar-footer'>
